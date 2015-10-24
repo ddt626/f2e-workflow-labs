518f5bec7a4c4124c8de4da8b0f404e75ad207d3
ddt626 <eightddt@gmail.com>
2015-10-24 15:07:27 +0800

    cb的實際用途說明

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['myTask1'], function(){
+gulp.task('default', ['myTask1', 'myTask2'], function(){
 	console.log('Hello Gulp');
 });
 
-gulp.task('myTask1', ['myTask2'], function(){
+gulp.task('myTask1', function(){
 	console.log('Hello task1');
 });
 
-gulp.task('myTask2', function(){
+gulp.task('myTask2', function(cb){
 	console.log('Hello task2');
 });
