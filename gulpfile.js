898a343af2445c9f84e6336b9bef5c02720063fe
ddt626 <eightddt@gmail.com>
2015-10-24 15:39:27 +0800

    建立 output1 工作，將所有 assets/vendor/bootstrap/**/*.js 檔案全部輸出到 output1 資料夾下

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,7 @@ gulp.task('myTask2', function(cb){
 	cb();
 });
 
-// 結果
-// λ gulp
-// [15:14:02] Using gulpfile D:\_課程\前端工程訓練\f2e-workflow-labs\gulpfile.js
-// [15:14:02] Starting 'myTask1'...
-// Hello task1
-// [15:14:02] Starting 'myTask2'...
-// Hello task2
-// [15:14:02] Finished 'myTask2' after 256 μs
+gulp.task('output1', function(){
+	gulp.src('assets/vendor/bootstrap/**/*.js')
+		.pipe(gulp.dest('output1'));
+});
