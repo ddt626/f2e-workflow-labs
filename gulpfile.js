63eb1fd3d5d39a5c5eac2cfa0885ea0046bd51af
ddt626 <eightddt@gmail.com>
2015-10-24 15:12:53 +0800

    在 mytask2 mytask2 mytask2 的 fn 加入 "cb " 參數，並在 fn 最後執行 cb ();

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,20 @@ gulp.task('myTask1', function(cb){
 	cb();
 });
 
-gulp.task('myTask2', function(){
+gulp.task('myTask2', function(cb){
 	console.log('Hello task2');
+	cb();
 });
+
+// 結果
+// λ gulp
+// [15:12:31] Using gulpfile D:\_課程\前端工程訓練\f2e-workflow-labs\gulpfile.js
+// [15:12:31] Starting 'myTask1'...
+// Hello task1
+// [15:12:31] Finished 'myTask1' after 202 μs
+// [15:12:31] Starting 'myTask2'...
+// Hello task2
+// [15:12:31] Finished 'myTask2' after 103 μs
+// [15:12:31] Starting 'default'...
+// Hello Gulp
+// [15:12:31] Finished 'default' after 894 μs
