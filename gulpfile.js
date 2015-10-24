44b7a27902bdf2c911c54df1a5ab2afba4a6e545
ddt626 <eightddt@gmail.com>
2015-10-24 15:18:42 +0800

    結果修正，在 mytask1 的 fn 加入 "cb" 參數，並在 fn 最後將 cb(); 註解，執行 default 工作，確認執行順序

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,9 @@ gulp.task('myTask2', function(cb){
 
 // 結果
 // λ gulp
-// [15:12:31] Using gulpfile D:\_課程\前端工程訓練\f2e-workflow-labs\gulpfile.js
-// [15:12:31] Starting 'myTask1'...
+// [15:14:02] Using gulpfile D:\_課程\前端工程訓練\f2e-workflow-labs\gulpfile.js
+// [15:14:02] Starting 'myTask1'...
 // Hello task1
-// [15:12:31] Finished 'myTask1' after 202 μs
-// [15:12:31] Starting 'myTask2'...
+// [15:14:02] Starting 'myTask2'...
 // Hello task2
-// [15:12:31] Finished 'myTask2' after 103 μs
-// [15:12:31] Starting 'default'...
-// Hello Gulp
-// [15:12:31] Finished 'default' after 894 μs
+// [15:14:02] Finished 'myTask2' after 256 μs
